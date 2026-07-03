7ec1c7e16bacd00ff7ffde530525fca3aee4b869
Yinell Echavarria <yinellabigail23@gmail.com>
2026-07-03 15:14:33 -0400

    Create main.c

---
+++ Tarea 6/9.13/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int cuenta(char);
+
+void main(void)
+{
+int res;
+char car;
+printf("\nIngrese el caracter que se va a buscar en el archivo: ");
+car = getchar();
+res = cuenta(car);
+if (res != -1)
+    printf("\n\nEl caracter %c sr encuentra en el archivo %d veces", car, res);
+else
+    printf("No se puede abrir el archivo");
+}
+
+int cuenta(char car)
+{
+int res, con = 0;
+char p;
+FILE *ar;
+if ((ar = fopen ("arc.txt", "r")) != NULL)
+
+{
+    while (!feof(ar))
+    {
+        p = getc(ar);
+        if (p == car)
+            con++;
+    }
+    fclose(ar);
+    res = con;
+}
+else
+    res = -1;
+return (res);
+}
